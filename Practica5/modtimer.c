4be7ac16983f87a8e26aebb72d21dc8fc3b46f7d
joalow <38871416+joalow@users.noreply.github.com>
2019-12-20 20:14:20 +0100

    Add files via upload

--- Practica5/modtimer.c
+++ Practica5/modtimer.c
@@ -3,6 +3,7 @@
 #define MAX_CBUFFER_LEN 32
 
 struct list_head mylist; // LISTA ENLAZADA
+int waitin = 0;
 
 // NODOS DE LA LISTA
 struct list_item {
@@ -12,7 +13,7 @@ struct list_item {
 
 struct kfifo cbuffer;		/* Buffer circular*/
 struct timer_list my_timer; // Estructura para implementar el temporizador
-struct work_struct *work;
+struct work_struct work;
 struct workqueue_struct* wq;
 
 spinlock_t sp_timer;
@@ -22,13 +23,14 @@ struct semaphore queue;
 struct proc_dir_entry* timer_proc; // Entrada /proc/modtimer
 struct proc_dir_entry* config_proc; // Entrada /proc/modconfig
 
-static unsigned long flags;
-
 static int timer_release(struct inode *inode, struct file *file);
 static int timer_open(struct inode *inode, struct file *file);
 static ssize_t timer_read(struct file *file, char *buff, size_t len, loff_t *offset);
-void timer_function(struct timer_list *);
-static void my_work_func(struct work_struct *work);
+void my_timer_function(unsigned long data);
+void my_work_func(struct work_struct *work);
+
+void cleanup_modul(void);
+int init_modul(void);
 
 static struct file_operations timer_fops = {
     .owner = THIS_MODULE,
@@ -37,80 +39,72 @@ static struct file_operations timer_fops = {
 	.release = timer_release,
 };
 
-static void my_work_func(struct work_struct *work)
+void my_work_func(struct work_struct *work)
 {
-	//copiar del buffer a array, y de array a lspinlockist
-	int* array = vmalloc(kfifo_len(&cbuffer));
+	//copiar del buffer a array, y de array a list
 	struct list_item* item = NULL;
-	int i;
+	int array[MAX_CBUFFER_LEN/4];	
+	int i, nElems;
+	unsigned long flags;
 
-
+	// Procedemos a pasarlo del buffer al array
 	spin_lock_irqsave(&sp_timer,flags);
 
-	kfifo_out(&cbuffer,array,kfifo_len(&cbuffer));
-	printk(KERN_INFO "volcado");
+	nElems = kfifo_len(&cbuffer);
+	printk(KERN_INFO "volcado de elems=%d",nElems);
+	i = kfifo_out(&cbuffer,&array,nElems);
 
 	spin_unlock_irqrestore(&sp_timer,flags);
-	
-	// Procedemos a pasarlo a la lista enlazada
-	for(i = 0; i < kfifo_len(&cbuffer); i++){
+
+	for (i = 0; i < nElems/4; i++)
+		printk(KERN_INFO "%d,",array[i]);
+
+	if(down_interruptible(&sem_list))
+		return (void)-EINTR;
+	// Procedemos a pasarlo del array a la lista enlazada
+	for(i = 0; i < 	nElems/4; i++){ // Dividimos entre 4 porque cuando alcanza el % de ocupacion salen 24 pero son bytes
 		item = (struct list_item*) vmalloc(sizeof(struct list_item));
 		item->data = array[i];
-		if(down_interruptible(&sem_list))
-			return -EINTR;
-		printk(KERN_INFO "add %d", item->data);
 		list_add_tail(&item->links,&mylist);
-		up(&sem_list);
-		up(&queue); //cola de espera de lista enlazada vacia
 	}
+	if(waitin && nElems/4 > 0)
+		up(&queue); //cola de espera de lista enlazada vacia
+	up(&sem_list);
 }
 
 // Generar numero aleatorio y meterlo al buffer circular 
 void my_timer_function(unsigned long data){
+	unsigned long flags = 0;
 	int n = get_random_int() % max_random;
+	printk(KERN_INFO "random=%d",n);
 
 	spin_lock_irqsave(&sp_timer,flags);
 
-	printk(KERN_INFO "numero del timer: %d", n);
-	kfifo_in(&cbuffer, n, sizeof(n));
+	kfifo_in(&cbuffer, &n, sizeof(n));
+
+	//posible down_interruptible(&sem_list)
+	if((kfifo_len(&cbuffer)*100)/kfifo_size(&cbuffer) >= emergency_threshold) //% de ocupacion alcanzado
+		queue_work(wq, &work);
 
 	spin_unlock_irqrestore(&sp_timer,flags);
-
-	if(kfifo_len(&cbuffer)/kfifo_size(&cbuffer) >= emergency_threshold){ //% de ocupacion alcanzado
-		queue_work(wq,work);
-		flush_work(work);
-	}
-}
-
-// Funcion que usamos para incializar todo lo relativo al timer
-static void init_my_timer(void){
-	init_timer(&my_timer);
-	printk(KERN_INFO "iniciado timer");
-	my_timer.expires = jiffies + timer_period_ms; // EXPIRES ==> Tiempo de activacion del timer ======== timer_period_ms ????
-	my_timer.data = 0;
-	my_timer.function = my_timer_function;
-
-	printk(KERN_INFO "iniciado timer aun");
-	// AQUI ACTIVAMOS EL TIMER POR PRIMERA VEZ, EMPEZAMOS LA CUENTA
-	add_timer(&my_timer);
-			printk(KERN_INFO "iniciado timer es");
+	mod_timer(&my_timer, jiffies + HZ);
 }
 
 /* Funciones de inicialización y descarga del módulo*/
 int init_modul(void){
 	// CREAMOS Y DEFINIMOS EL BUFFER CIRCULAR
-	printk(KERN_INFO "iniciando");
-	kfifo_alloc(&cbuffer,MAX_CBUFFER_LEN,GFP_KERNEL);
+	if(kfifo_alloc(&cbuffer,MAX_CBUFFER_LEN,GFP_KERNEL))
+		return -EINTR;
 
 	// INICIALIZAMOS LA LISTA ENLAZADA
 	INIT_LIST_HEAD(&mylist);
-	printk(KERN_INFO "iniciado aun");
+
     sema_init(&sem_list,1);
 	sema_init(&queue,0);
 	spin_lock_init(&sp_timer);
 
 	// CREAMOS LA WORKQUEUE
-	INIT_WORK(work,my_work_func);
+	INIT_WORK(&work,my_work_func);
 	wq = create_workqueue("bottom_half");
 
 	// INICIALIZAMOS LAS VARIABLES
@@ -124,13 +118,19 @@ int init_modul(void){
 		goto failed;
 
 	// CREAMOS E INICIALIZAMOS EL TIMER
-	init_my_timer();
-	printk(KERN_INFO "iniciado");
+	init_timer(&my_timer);
+	my_timer.expires = jiffies + timer_period_ms; // EXPIRES ==> Tiempo de activacion del timer ======== timer_period_ms ????
+	my_timer.data = 0;
+	my_timer.function = my_timer_function;
+
+	// AQUI ACTIVAMOS EL TIMER POR PRIMERA VEZ, EMPEZAMOS LA CUENTA
+	add_timer(&my_timer);
+
 	return 0;
 
 	failed:
-		printk(KERN_INFO "no iniciado");
 		kfifo_free(&cbuffer);
+		destroy_workqueue(wq);
 		return -EAGAIN; // Error: ty again
 }
 
@@ -138,18 +138,20 @@ void cleanup_modul(void){
 	struct list_item* item = NULL;
 	struct list_head* cur_node = NULL;
 	struct list_head* aux = NULL;
+	unsigned long flags = 0;
 	
 	del_timer_sync(&my_timer);
-
 	// Esperar a que acaben los trabajos de la workqueue creada
-	flush_scheduled_work();
+	flush_workqueue(wq);
 
 	// ELIMINAMOS LAS ENTRADAS /proc
 	remove_proc_entry("modtimer",NULL);
 	remove_proc_entry("modconfig",NULL);
 
-	if(!list_empty(&mylist)){ // Ponemos if porque la funcion list_for_each_safe() recorre la lista asique no hace falta hacer mas iteraciones
+	if(down_interruptible(&sem_list))
+		return (void)-EINTR;
 
+	if(!list_empty(&mylist)){ // Ponemos if porque la funcion list_for_each_safe() recorre la lista asique no hace falta hacer mas iteraciones
 		//En aux almacenamos el siguiente nodo de la lista y cur_node es el nodo a borrar (???)
 		list_for_each_safe(cur_node, aux, &mylist) {
 			item = list_entry(cur_node, struct list_item, links);//&mylist);
@@ -157,32 +159,19 @@ void cleanup_modul(void){
 			vfree(item);
 		};
 	}
+	up(&sem_list);
+
+	spin_lock_irqsave(&sp_timer,flags);
 	kfifo_free(&cbuffer);
+	spin_unlock_irqrestore(&sp_timer,flags);
+	destroy_workqueue(wq);
 }
 
 /* Se invoca al hacer close() de entrada /proc */
 static int timer_release (struct inode *inode, struct file *file){
-	// DESACTIVAMOS EL TEMPORIZADOR
-	struct list_item* item = NULL;
-	struct list_head* cur_node = NULL;
-	struct list_head* aux = NULL;
-	del_timer_sync(&my_timer);
-	
-	// ESPERAR A QUE ACABEN TODOS LOS TRABAJOS DE LA WORKQUEUE
-	flush_scheduled_work();
-
-	// VACIAMOS EL BUFFER CIRCULAR (supone eliminar todos sus elementos)
-	kfifo_reset(&cbuffer);
-
-	// VACIAR LA LISTA
-	list_for_each_safe(cur_node, aux, &mylist){
-		item = list_entry(cur_node, struct list_item, links);
-		list_del(cur_node);
-		vfree(item);
-	}
-
-	// DECREMENTAR EN UNO EL CONTADOR INTERNO DE REFERENCIAS
 	module_put(THIS_MODULE);
+
+	return 0;
 }
 
 /* Se invocaal hacer open() de entrada /proc */
@@ -201,18 +190,43 @@ static ssize_t timer_read(struct file *file, char *buff, size_t len, loff_t *off
 	struct list_item* item = NULL;
 	struct list_head* aux = NULL;
 	struct list_head* cur_node = NULL;
-	//char* kbuff = vmalloc(sizeof(int));
+	char* kbuff = NULL;
+	char* kbuff_aux = NULL;
+	int ret = 0;
+	int nums = (((MAX_CBUFFER_LEN/4)*75)/100);
 
+	if(list_empty(&mylist)){
+		waitin = 1;
+		if(down_interruptible(&queue))
+			return -EINTR;
+	}
+	
+	if(down_interruptible(&sem_list))
+		return -EINTR;
+
+	kbuff = (char*) vmalloc( (nums*sizeof(int))+nums );
+	printk(KERN_INFO "VA A ENTRAR");
 	list_for_each_safe(cur_node, aux, &mylist) {
-		char* kbuff = vmalloc(sizeof(int));
+		kbuff_aux = (char*) vmalloc(sizeof(int)+1);
 		item = list_entry(cur_node, struct list_item, links);
-		sprintf(kbuff,"%d",item->data);
-		copy_to_user(buff,kbuff,sizeof(int));
+		ret += sprintf(kbuff_aux,"%d\n",item->data);
+		strcat(kbuff,kbuff_aux);
 		list_del(cur_node);
 		vfree(item);
-		vfree(kbuff);
+		vfree(kbuff_aux);
 	};
-	
+	if(copy_to_user(buff,kbuff,ret +(ret/4)))
+		goto fail;
+	vfree(kbuff);
+	up(&sem_list);	
+	printk(KERN_INFO "RET=%d",ret);
+	return ret;
+
+	fail:
+		vfree(item);
+		vfree(kbuff);
+		up(&sem_list);
+		return -EINTR;
 }
 
 module_init(init_modul);
